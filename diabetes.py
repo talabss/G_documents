1a4310aa2b7b593dea40a0018d2fc286224dc6e6
talabi oluwatomisin <oriyomitalabss@gmail.com>
2025-06-16 05:17:42 -0700

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -10,8 +10,21 @@ import seaborn as sns
 #convert file to dataframe
 
 df = pd.read_csv("diabetes.csv")
-st.markdown("## overview")
-st.write(df.info())
+st.markdown("# first five iteams")
+st.write(df.head())
+
+st.markdown('# last ten items')
+st.write(df.tall(10))
+
+st.title("general information about diabetes analysis")
+hall = df.describe
+st.write(hall)
+
+st.title('blood pressure chart')
+counted = df["blood pressure"].value_counts().reset_index()
+counted,columns = ["bloodpressue", "count"]
+bloodpressure = px,pie(counted, names = "bloodpressure",values =
+st.plotly_chart('bloodpressure', use_container_width = True)
 
 #import -m pip install scikit-learn
 #import -m pip install matlib
